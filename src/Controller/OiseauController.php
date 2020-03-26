99b6de1b8237baed685c80fd0e556a6492947dd4
blandine-dev <viry.blandine@gmail.com>
2020-03-26 11:16:21 +0100

    liens bootstrap et création des routes accueil contact apropos et navbar

--- src/Controller/OiseauController.php
+++ src/Controller/OiseauController.php
@@ -7,6 +7,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OiseauController extends AbstractController
 {
+     /**
+     * @Route("/", name="home")
+     */
+    public function accueil()
+    {
+       
+        return $this->render('oiseau/accueil.html.twig');
+    }
     /**
      * @Route("/oiseau", name="oiseau")
      */
@@ -15,5 +23,23 @@ class OiseauController extends AbstractController
         return $this->render('oiseau/index.html.twig', [
             'controller_name' => 'OiseauController',
         ]);
+    }
+
+     /**
+     * @Route("/apropos", name="apropos")
+     */
+    public function apropos()
+    {
+       
+        return $this->render('oiseau/apropos.html.twig');
+    }
+
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+       
+        return $this->render('oiseau/contact.html.twig');
     }
 }
